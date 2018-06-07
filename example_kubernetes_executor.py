b409a0ecef1b7208b12077a02003174fd9bbdf92
simisoz <simiso.mike@gmail.com>
2018-06-07 22:17:18 +0200

    Update example_kubernetes_executor.py

--- example_kubernetes_executor.py
+++ example_kubernetes_executor.py
@@ -50,13 +50,13 @@ start_task = PythonOperator(
 # But you can if you want to
 one_task = PythonOperator(
     task_id="one_task", python_callable=print_stuff, dag=dag,
-    executor_config={"KubernetesExecutor": {"image": "airflow/ci_zip:latest"}}
+    executor_config={"KubernetesExecutor": {"image": "foundery/airflow:latest"}}
 )
 
 # Use the zip binary, which is only found in this special docker image
 two_task = PythonOperator(
     task_id="two_task", python_callable=use_zip_binary, dag=dag,
-    executor_config={"KubernetesExecutor": {"image": "airflow/ci_zip:latest"}}
+    executor_config={"KubernetesExecutor": {"image": "foundery/airflow:latest"}}
 )
 
 # Limit resources on this operator/task

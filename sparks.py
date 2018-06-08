59ed3f567131cc42dbe9e75d25b30c750379818a
simisoz <simiso.mike@gmail.com>
2018-06-08 09:56:53 +0200

    Update sparks.py

--- sparks.py
+++ sparks.py
@@ -37,17 +37,17 @@ t1 = BashOperator(
 )
 
 t3 = BashOperator(
-    task_id="delete_spark", dag=dag, bash_command="sleep 30 && helm init --client-only && helm del --purge spark",
+    task_id="delete_spark", dag=dag, bash_command="sleep 1800 && helm init --client-only && helm del --purge spark",
     executor_config={"KubernetesExecutor": {
         "image": "foundery/airflow_helm:latest"}}
 )
 
-#t2 = BashOperator(
-#    task_id="process_spark", dag=dag, bash_command="sleep 30 && helm init --client-only &&  ./bin/spark-submit --class org.apache.spark.examples.SparkPi --master 192.168.99.101:8443 --deploy-mode cluster --executor-memory 1G --num-executors 5 $SPARK_HOME/examples/jars/spark-example_2.11-2.3.0.jar",
-#    executor_config={"KubernetesExecutor": {
-#        "image": "foundery/airflow_spark:latest"}}
-#)
+t2 = BashOperator(
+    task_id="process_spark", dag=dag, bash_command="sleep 120 && ./bin/spark-submit --class org.apache.spark.examples.SparkPi --master 192.168.99.101:8443 --deploy-mode cluster --executor-memory 1G --num-executors 5 $SPARK_HOME/examples/jars/spark-example_2.11-2.3.0.jar",
+    executor_config={"KubernetesExecutor": {
+        "image": "foundery/airflow_spark:latest"}}
+)
 
 
-t1 >> t3
+t1 >> t2>> t3
 

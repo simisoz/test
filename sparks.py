f6ca13f965cb2d0210abc92a30b70a770d72e900
simisoz <simiso.mike@gmail.com>
2018-06-08 09:42:32 +0200

    Update sparks.py

--- sparks.py
+++ sparks.py
@@ -19,14 +19,14 @@ import datetime as dt
 
 default_args = {
     'owner': 'me',
-    'start_date': dt.datetime(2018, 4, 4),
+    'start_date': None,
     'retries': 1,
     'retry_delay': dt.timedelta(minutes=5),
 }
 
 dag = DAG(
     dag_id='spark', default_args=default_args,
-    schedule_interval='0 0 * * *'
+    schedule_interval='@once'
 )
 
 
@@ -42,11 +42,11 @@ t3 = BashOperator(
         "image": "foundery/airflow_helm:latest"}}
 )
 
-t2 = BashOperator(
-    task_id="process_spark", dag=dag, bash_command="sleep 30 && helm init --client-only &&  ./bin/spark-submit --class org.apache.spark.examples.SparkPi --master 192.168.99.101:8443 --deploy-mode cluster --executor-memory 1G --num-executors 5 $SPARK_HOME/examples/jars/spark-example_2.11-2.3.0.jar",
-    executor_config={"KubernetesExecutor": {
-        "image": "foundery/airflow_spark:latest"}}
-)
+#t2 = BashOperator(
+#    task_id="process_spark", dag=dag, bash_command="sleep 30 && helm init --client-only &&  ./bin/spark-submit --class org.apache.spark.examples.SparkPi --master 192.168.99.101:8443 --deploy-mode cluster --executor-memory 1G --num-executors 5 $SPARK_HOME/examples/jars/spark-example_2.11-2.3.0.jar",
+#    executor_config={"KubernetesExecutor": {
+#        "image": "foundery/airflow_spark:latest"}}
+#)
 
 
 t1 >> t3

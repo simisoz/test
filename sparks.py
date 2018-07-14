4796ba2a066da1f2c638f632adee8b29503c20c5
simiso <simiso.mike@gmail.com>
2018-07-14 20:47:08 +0200

    sparks.py

--- sparks.py
+++ sparks.py
@@ -1,17 +1,3 @@
-# -*- coding: utf-8 -*-
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 from airflow.operators.bash_operator import BashOperator
 from airflow import DAG
 import datetime as dt
@@ -33,21 +19,21 @@ dag = DAG(
 t1 = BashOperator(
     task_id="spawn_spark", dag=dag, bash_command="sleep 30 && helm init --client-only && helm install --name spark stable/spark",
     executor_config={"KubernetesExecutor": {
-        "image": "foundery/airflow_helm:latest"}}
+        "image": "helm:latest"}}
+)
+
+
+t2 = BashOperator(
+    task_id="process_spark", dag=dag, bash_command="spark-submit --class org.apache.spark.examples.SparkPi --master https://192.168.39.191:8443 --deploy-mode cluster --executor-memory 1G --num-executors 3 $SPARK_HOME/examples/jars/spark-example_2.11-2.3.0.jar",
+    executor_config={"KubernetesExecutor": {
+        "image": "spark23:latest"}}
 )
 
 t3 = BashOperator(
-    task_id="delete_spark", dag=dag, bash_command="sleep 1800 && helm init --client-only && helm del --purge spark",
+    task_id="delete_spark", dag=dag, bash_command="helm init --client-only && helm delete --purge spark",
     executor_config={"KubernetesExecutor": {
-        "image": "foundery/airflow_helm:latest"}}
+        "image": "helm:latest"}}
 )
 
-t2 = BashOperator(
-    task_id="process_spark", dag=dag, bash_command="sleep 120 && ./bin/spark-submit --class org.apache.spark.examples.SparkPi --master 192.168.99.101:8443 --deploy-mode cluster --executor-memory 1G --num-executors 5 $SPARK_HOME/examples/jars/spark-example_2.11-2.3.0.jar",
-    executor_config={"KubernetesExecutor": {
-        "image": "foundery/airflow_spark:latest"}}
-)
-
-
-t1 >> t2>> t3
+t1 >> t2
 

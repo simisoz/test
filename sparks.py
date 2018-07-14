753267f504f3f2a9e189eb89f3ee0daa1b38ebe9
simiso <simiso.mike@gmail.com>
2018-07-14 21:21:52 +0200

    Fix test Dag

--- sparks.py
+++ sparks.py
@@ -19,20 +19,23 @@ dag = DAG(
 t1 = BashOperator(
     task_id="spawn_spark", dag=dag, bash_command="sleep 30 && helm init --client-only && helm install --name spark stable/spark",
     executor_config={"KubernetesExecutor": {
-        "image": "helm:latest"}}
+        "image": "helm:latest",
+        "namespace": "airflow-ke"}}
 )
 
 
 t2 = BashOperator(
     task_id="process_spark", dag=dag, bash_command="spark-submit --class org.apache.spark.examples.SparkPi --master k8s://https://192.168.39.191:8443 --deploy-mode cluster --executor-memory 1G --num-executors 3 $SPARK_HOME/examples/jars/spark-example_2.11-2.3.0.jar",
     executor_config={"KubernetesExecutor": {
-        "image": "spark23:latest"}}
+        "image": "spark23:latest",
+        "namespace": "airflow-ke"}}
 )
 
 t3 = BashOperator(
     task_id="delete_spark", dag=dag, bash_command="helm init --client-only && helm delete --purge spark",
     executor_config={"KubernetesExecutor": {
-        "image": "helm:latest"}}
+        "image": "helm:latest",
+        "namespace": "airflow-ke"}}
 )
 
 t1 >> t2 >> t3

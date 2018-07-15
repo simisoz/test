d9988efcee53b2bc89a5ee6d2de16d1525db62f0
simiso <simiso.mike@gmail.com>
2018-07-15 21:08:26 +0200

    Add Spark Submit Operator

--- sparks.py
+++ sparks.py
@@ -1,13 +1,30 @@
-from airflow.operators.bash_operator import BashOperator
-from airflow import DAG
 import datetime as dt
-
+from airflow import DAG
+from airflow.contrib.operators.spark_submit_operator import SparkSubmitOperator
+from airflow.operators.bash_operator import BashOperator
 
 default_args = {
     'owner': 'me',
     'start_date': dt.datetime(2018, 6, 8),
     'retries': 1,
     'retry_delay': dt.timedelta(minutes=5),
+}
+submit_config = {
+    'conf': {
+        'spark.kubernetes.container.image': 'spark23:latest'
+    },
+    'total_executor_cores': 4,
+    'executor_cores': 1,
+    'executor_memory': '1g',
+    'name': '{{ task_instance.task_id }}',
+    'num_executors': 4,
+    'verbose': True,
+    'driver_memory': '1g',
+}
+executor_config = {
+    "KubernetesExecutor": {
+    "image": "helm:latest"
+    }
 }
 
 dag = DAG(
@@ -16,27 +33,30 @@ dag = DAG(
 )
 
 
-t1 = BashOperator(
+spawn_spark = BashOperator(
     task_id="spawn_spark", dag=dag, bash_command="sleep 30 && helm init --client-only && helm install --name spark stable/spark",
-    executor_config={"KubernetesExecutor": {
-        "image": "helm:latest",
-        "namespace": "airflow-ke"}}
+    executor_config=executor_config)
+
+# t2 = BashOperator(
+#     task_id="process_spark", dag=dag, bash_command="spark-submit --class org.apache.spark.examples.SparkPi --master k8s://https://192.168.39.191:8443 --deploy-mode cluster --executor-memory 1G --num-executors 3 --conf spark.kubernetes.container.image=spark23:latest  local:///$SPARK_HOME/examples/jars/spark-example_2.11-2.3.0.jar",
+#     executor_config = {"KubernetesExecutor": {
+#         "image": "spark23:latest",
+#         "namespace": "airflow-ke"}}
+# )
+
+compute_pi = SparkSubmitOperator(
+    task_id='compute_pi',
+    conn_id='spark_kubernetes',
+    application='local:///$SPARK_HOME/examples/jars/spark-example_2.11-2.3.0.jar"',
+    java_class='org.apache.spark.examples.SparkPi',
+    dag=dag,
+    executor_config={"KubernetesExecutor": {"image": "spark23:latest"}},
+    **submit_config
 )
 
 
-t2 = BashOperator(
-    task_id="process_spark", dag=dag, bash_command="spark-submit --class org.apache.spark.examples.SparkPi --master k8s://https://192.168.39.191:8443 --deploy-mode cluster --executor-memory 1G --num-executors 3 --conf spark.kubernetes.container.image=spark23:latest  local:///$SPARK_HOME/examples/jars/spark-example_2.11-2.3.0.jar",
-    executor_config={"KubernetesExecutor": {
-        "image": "spark23:latest",
-        "namespace": "airflow-ke"}}
-)
-
-t3 = BashOperator(
+delete_spark = BashOperator(
     task_id="delete_spark", dag=dag, bash_command="helm init --client-only && helm delete --purge spark",
-    executor_config={"KubernetesExecutor": {
-        "image": "helm:latest",
-        "namespace": "airflow-ke"}}
-)
+    executor_config=executor_config)
 
-t1 >> t2 >> t3
-
+spawn_spark >> compute_pi >> delete_spark

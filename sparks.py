7b7f6a6fe65f28d805492e4c43f0588570659743
simiso <simiso.mike@gmail.com>
2018-07-16 02:01:19 +0200

    Hide Steps

--- sparks.py
+++ sparks.py
@@ -33,9 +33,9 @@ dag = DAG(
 )
 
 
-spawn_spark = BashOperator(
-    task_id="spawn_spark", dag=dag, bash_command="sleep 30 && helm init --client-only && helm install --name spark stable/spark:0.1.14",
-    executor_config=executor_config)
+# spawn_spark = BashOperator(
+#     task_id="spawn_spark", dag=dag, bash_command="sleep 30 && helm init --client-only && helm install --name spark stable/spark:0.1.14",
+#     executor_config=executor_config)
 
 # t2 = BashOperator(
 #     task_id="process_spark", dag=dag, bash_command="spark-submit --class org.apache.spark.examples.SparkPi --master k8s://https://192.168.39.191:8443 --deploy-mode cluster --executor-memory 1G --num-executors 3 --conf spark.kubernetes.container.image=spark23:latest  local:///$SPARK_HOME/examples/jars/spark-example_2.11-2.3.0.jar",
@@ -55,8 +55,8 @@ compute_pi = SparkSubmitOperator(
 )
 
 
-delete_spark = BashOperator(
-    task_id="delete_spark", dag=dag, bash_command="helm init --client-only && helm delete --purge spark",
-    executor_config=executor_config)
+# delete_spark = BashOperator(
+#     task_id="delete_spark", dag=dag, bash_command="helm init --client-only && helm delete --purge spark",
+#     executor_config=executor_config)
 
 compute_pi 

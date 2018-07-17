00bc08d9206818ded586f94cef82402c66c23ab3
simiso <simiso.mike@gmail.com>
2018-07-17 11:59:23 +0200

    POC

--- sparks.py
+++ sparks.py
@@ -2,20 +2,28 @@ import datetime as dt
 from airflow import DAG
 from airflow.contrib.operators.spark_submit_operator import SparkSubmitOperator
 from airflow.operators.bash_operator import BashOperator
-from airflow.operators.slack_operator import SlackAPIPostOperator
-from airflow.operators.python_operator import PythonOperator
-# from kubernetes import client, config
 
 default_args = {
     'owner': 'me',
-    'start_date': dt.datetime(2018, 7, 8),
+    'start_date': dt.datetime(2018, 6, 8),
     'retries': 1,
     'retry_delay': dt.timedelta(minutes=5),
 }
-
+submit_config = {
+    'conf': {
+        'spark.kubernetes.container.image': 'spark23:latest'
+    },
+    'total_executor_cores': 4,
+    'executor_cores': 1,
+    'executor_memory': '1g',
+    'name': '{{ task_instance.task_id }}',
+    'num_executors': 4,
+    'verbose': True,
+    'driver_memory': '1g',
+}
 executor_config = {
     "KubernetesExecutor": {
-        "image": "helm:latest"
+    "image": "helm:latest"
     }
 }
 
@@ -25,42 +33,33 @@ dag = DAG(
 )
 
 
-# def spark_k8sservices(**context):
-#     api_instance = client.CoreV1Api(client.ApiClient(config.load_incluster_config()))
-#     return [(svc.spec.selector, svc.spec.ports[0].node_port) for svc in api_instance.items]
-
-# start_notification = SlackAPIPostOperator(
-#     task_id="slack_confirmation",
-#     channel="#airflow",
-#     username="airflow",
-#     token="xoxp-108470454706-107763802528-394935179685-7cafc5ed8dab3748ce2cc815c49e8cb5",
-#     text="Hello there",
-#     dag=dag,
-# )
-
 spawn_spark = BashOperator(
-    task_id="spawn_spark",
-    dag=dag,
+    task_id="spawn_spark", 
+    dag=dag, 
     bash_command="sleep 30 && helm init --client-only && helm install --name spark stable/spark --version=0.1.13 --namespace=spark",
-    executor_config=executor_config)
+    executor_config=executor_config
+    )
 
-# spark_k8sservices = PythonOperator(
-#     task_id='spark_k8sservices',
-#     dag=dag,
-#     python_callable=spark_k8sservices,
-#     executor_config=executor_config)
-
-# send_connections = SlackAPIPostOperator(
-#     task_id="send_connections",
-#     channel="#airflow",
-#     username="airflow",
-#     token="xoxp-108470454706-107763802528-394935179685-7cafc5ed8dab3748ce2cc815c49e8cb5",
-#     text="{{ ti.xcom_pull(task_ids='spark_k8sservices') }}",
-#     dag=dag,
+# t2 = BashOperator(
+#     task_id="process_spark", dag=dag, bash_command="spark-submit --class org.apache.spark.examples.SparkPi --master k8s://https://192.168.39.191:8443 --deploy-mode cluster --executor-memory 1G --num-executors 3 --conf spark.kubernetes.container.image=spark23:latest  local:///$SPARK_HOME/examples/jars/spark-example_2.11-2.3.0.jar",
+#     executor_config = {"KubernetesExecutor": {
+#         "image": "spark23:latest",
+#         "namespace": "airflow-ke"}}
 # )
+
+# compute_pi = SparkSubmitOperator(
+#     task_id='compute_pi',
+#     conn_id='spark_k8s_cluster',
+#     application='local:///usr/local/spark/examples/jars/spark-example_2.11-2.3.0.jar"',
+#     java_class='org.apache.spark.examples.SparkPi',
+#     dag=dag,
+#     executor_config={"KubernetesExecutor": {"image": "spark23:latest"}},
+#     **submit_config
+# )
+
 
 delete_spark = BashOperator(
     task_id="delete_spark", dag=dag, bash_command="helm init --client-only && helm delete --purge spark",
     executor_config=executor_config)
 
-spawn_spark  >>  delete_spark
+spawn_spark >>  delete_spark

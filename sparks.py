f736674bc62269540ee43c9713b56a4bb5995eb5
simisoz <simiso.mike@gmail.com>
2019-03-20 13:35:32 +0200

    Update sparks.py

--- sparks.py
+++ sparks.py
@@ -3,7 +3,6 @@ from airflow import DAG
 from airflow.contrib.operators.spark_submit_operator import SparkSubmitOperator
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
-# from kubernetes import client, config
 
 default_args = {
     'owner': 'me',
@@ -42,30 +41,11 @@ spawn_spark = BashOperator(
     executor_config=executor_config
     )
 
-# t2 = BashOperator(
-#     task_id="process_spark", dag=dag, bash_command="spark-submit --class org.apache.spark.examples.SparkPi --master k8s://https://192.168.39.191:8443 --deploy-mode cluster --executor-memory 1G --num-executors 3 --conf spark.kubernetes.container.image=spark23:latest  local:///$SPARK_HOME/examples/jars/spark-example_2.11-2.3.0.jar",
-#     executor_config = {"KubernetesExecutor": {
-#         "image": "spark23:latest",
-#         "namespace": "airflow-ke"}}
-# )
-
-# def spark_k8sservices(**context):
-#     api_instance = client.CoreV1Api(client.ApiClient(config.load_incluster_config()))
-#     api_response = api_instance.list_namespaced_service(namespace="spark", watch=False)
-#     # all_services = [ sc.api_call("chat.postMessage",channel="#airflow", text=str("Load Balancer Port",svc.spec.ports[0].node_port)) for svc in api_response.items]
-#     return [(svc.spec.selector, svc.spec.ports[0].node_port) for svc in api_response.items]
-
-# spark_k8sservices = PythonOperator(
-#     task_id='spark_k8sservices',
-#     dag=dag,
-#     python_callable=spark_k8sservices,
-#     executor_config=executor_config)
-
 
 delete_spark = BashOperator(
     task_id="delete_spark", 
     dag=dag, 
-    bash_command="helm init --client-only && helm delete --purge spark",
+    bash_command="sleep 300 ; helm init --client-only && helm delete --purge spark",
     executor_config=executor_config)
 
-spawn_spark 
+spawn_spark  >> delete_spark
